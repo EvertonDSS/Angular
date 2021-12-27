650611953c176aaaff729d078e49dc6142fbedd8
Everton David <everton.097@gmail.com>
2021-12-27 18:18:32 -0300

    mudando bytebank

--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -18,5 +18,11 @@ export class NovaTransferenciaComponent {
     //console.log('Destino: ', this.destino)
     const valorEmitir = {valor: this.valor, destino: this.destino};
     this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = null;
+    this.destino = null;
   }
 }
